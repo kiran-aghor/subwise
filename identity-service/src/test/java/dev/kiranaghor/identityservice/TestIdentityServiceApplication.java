311cae656ce51a1ba88affa343ea17afee21d904
kiran aghor <kiran.aghor@gmail.com>
2025-04-20 15:30:47 +0800

    format code spotless plugin

--- identity-service/src/test/java/dev/kiranaghor/identityservice/TestIdentityServiceApplication.java
+++ identity-service/src/test/java/dev/kiranaghor/identityservice/TestIdentityServiceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 
 public class TestIdentityServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(IdentityServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(IdentityServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
 }
