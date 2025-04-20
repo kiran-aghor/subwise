7590deb5af769fc265c1093b8330a5955e54471c
kiran aghor <kiran.aghor@ilex.sg>
2025-04-20 13:15:06 +0800

    setup Subwise backend project and add indentity service module

---
+++ identity-service/src/test/java/dev/kiranaghor/identityservice/TestIdentityServiceApplication.java
@@ -0,0 +1,11 @@
+package dev.kiranaghor.identityservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(IdentityServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
